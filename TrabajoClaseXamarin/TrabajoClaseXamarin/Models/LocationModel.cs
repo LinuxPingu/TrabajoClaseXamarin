8f8780ef4eb409ee71b58310aa5d75549d1d04a4
LinuxPingu <47802332+LinuxPingu@users.noreply.github.com>
2021-01-13 18:16:18 -0600

    Models

---
+++ TrabajoClaseXamarin/TrabajoClaseXamarin/Models/LocationModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrabajoClaseXamarin.Models
+{
+    public class LocationModel
+    {
+
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string TimeZone { get; set; }
+
+        public LocationModel()
+        {
+        }
+
+        public LocationModel(string street, string city, string state, string country, string timeZone)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            Country = country;
+            TimeZone = timeZone;
+        }
+    }
+}
